5bf065d75262b023f5c30fcb2e49bc0a36ecd3a7
gnayiqnat <133224159+gnayiqnat@users.noreply.github.com>
2024-06-23 19:38:10 +0800

    Pages - Add page transition

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,17 +1,23 @@
-
-import { Navbar } from "@/components/navbar";
+import { Navbar } from '@/components/navbar';
+import { motion } from 'framer-motion';
 
 export default function DefaultLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <div className="relative flex flex-col h-screen">
-      <Navbar />
-      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
-        {children}
-      </main>
-    </div>
-  );
+	return (
+		<div className='relative flex flex-col h-screen overflow-hidden'>
+			<Navbar />
+			<motion.div
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 1 }}
+				transition={{ delay: 0.25, duration: 0.5 }}
+			>
+				<main className='container mx-auto max-w-7xl px-6 flex-grow pt-16'>
+					{children}
+				</main>
+			</motion.div>
+		</div>
+	);
 }
