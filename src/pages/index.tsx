7dd11d21052b416413e15215f6cc058b0e210929
gnayiqnat <133224159+gnayiqnat@users.noreply.github.com>
2024-06-23 20:24:13 +0800

    Add buttons to hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import { Spotlight } from '@/components/ui/Spotlight';
 import DefaultLayout from '@/layouts/default';
+import { Button } from '@nextui-org/button';
+import { motion } from 'framer-motion';
+import { useNavigate } from 'react-router-dom';
 
 export default function IndexPage() {
+	const navigate = useNavigate();
+
 	return (
 		<>
 			<div className='h-lvh'>
@@ -16,10 +21,63 @@ export default function IndexPage() {
 							<h1 className='py-3 text-4xl md:text-6xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-600 bg-opacity-50'>
 								Digital Ascent
 							</h1>
-							<h4 className='text-default-400 text-xl pt-2'>
+							<h4 className='text-default-500 text-xl pt-2'>
 								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed
 								totam error omnis, atque distinctio modi facere?
 							</h4>
+							<br />
+							<div className='mt-6 flex items-center justify-center gap-3 opacity-90'>
+								<motion.div
+									initial={{ opacity: 1 }}
+									whileHover={{ opacity: 0.8 }}
+								>
+									<Button
+										color='primary'
+										onPress={() => {
+											navigate('/games');
+										}}
+										style={{
+											opacity: 0.75,
+											padding: '7px 16px',
+											height: '45px',
+											borderRadius: '3px',
+										}}
+									>
+										<p
+											style={{
+												color: '#000000',
+												fontSize: '16px',
+											}}
+										>
+											Explore our Games
+										</p>
+									</Button>
+								</motion.div>
+
+								<Button
+									variant='light'
+									size='lg'
+									color='primary'
+									onPress={() => {
+										navigate('/about');
+									}}
+									style={{
+										opacity: 0.85,
+										padding: '7px 16px',
+										height: '45px',
+										borderRadius: '3px',
+									}}
+								>
+									<p
+										style={{
+											color: '#ffffff',
+											fontSize: '16px',
+										}}
+									>
+										Learn More <span className='ml-1 text-lg'> {''}</span>
+									</p>
+								</Button>
+							</div>
 						</div>
 					</section>
 				</DefaultLayout>
