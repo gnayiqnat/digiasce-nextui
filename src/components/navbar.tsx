a213701610e1ce4be515ef3fb13e6749f5bf6a65
Tan Qi Yang <contact@tanqiyang.com>
2024-07-06 21:46:09 +0800

    Fix: Navbar menu button display issue
    Adjusted tailwind CSS styles to ensure proper display of the menu button at specific screen widths.
    
    File(s) modified: src\components\navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -52,7 +52,7 @@ export const Navbar = () => {
 			</NavbarContent>
 
 			<NavbarContent
-				className='hidden sm:flex basis-1/5 sm:basis-full'
+				className='hidden lg:flex  basis-1/5 sm:basis-full'
 				justify='end'
 			>
 				<NavbarItem className='hidden sm:flex gap-2'>
@@ -62,7 +62,7 @@ export const Navbar = () => {
 				</NavbarItem>
 			</NavbarContent>
 
-			<NavbarContent className='sm:hidden basis-1 pl-4' justify='end'>
+			<NavbarContent className='lg:hidden basis-1 pl-4' justify='end'>
 				<Link isExternal href={siteConfig.links.github}>
 					<GithubIcon className='text-default-500' />
 				</Link>
