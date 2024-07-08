1cda38ad85fc41f89babced50da200af0eda341c
Tan Qi Yang <contact@tanqiyang.com>
2024-07-08 16:50:00 +0800

    Code cleanup

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,3 @@
-import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
 
 export default function AboutPage() {
@@ -6,7 +5,7 @@ export default function AboutPage() {
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>About</h1>
+          <h1 className='text-4xl'>About</h1>
         </div>
       </section>
     </DefaultLayout>
