5b059060f8fe34562d8a8976f29a585fb1a693f7
Tan Qi Yang <contact@tanqiyang.com>
2024-12-17 08:35:39 +0800

    add Salvaage game to /games

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,10 +1,5 @@
 import DefaultLayout from '@/layouts/default';
-import {
-	Card,
-	CardFooter,
-	Chip,
-	Image
-} from '@nextui-org/react';
+import { Card, CardFooter, Chip, Image } from '@nextui-org/react';
 import { motion } from 'framer-motion';
 
 export default function GamesPage() {
@@ -14,48 +9,69 @@ export default function GamesPage() {
 				<div className='inline-block max-w-lg text-center justify-center'>
 					<h1 className='text-4xl pl-0 md:pl-3 pb-4 md:pb-7'>Games</h1>
 				</div>
-				<GameSection />
+				<div className='flex flex-col sm:flex-row items-center justify-center gap-9'>
+					<GameCard
+						gameName='Down to Earth'
+						gameDesc='A single-player game centered around the theme of green energy.'
+						gameTag={['First-Person Shooter', 'Adventure']}
+						image='/dte-cover.webp'
+						url='https://www.roblox.com/games/15127205237/'
+					/>
+					<GameCard
+						gameName='Salvaage'
+						gameDesc='An action-adventure game centered around waste management.'
+						gameTag={['Action', 'Adventure','Level-based']}
+						image='/salvaage-cover.png'
+						url='https://www.roblox.com/games/18501337793/'
+					/>
+				</div>
 			</section>
 		</DefaultLayout>
 	);
 }
 
-export function GameSection() {
+function GameCard({
+	gameName,
+	gameDesc,
+	gameTag,
+	image,
+	url,
+}: {
+	gameName: string;
+	gameDesc: string;
+	gameTag: Array<string>;
+	image: string;
+	url: string;
+}) {
 	return (
 		<>
-			<div className='flex flex-col sm:flex-row items-center justify-center gap-9'>
-				<motion.div initial={{ scale: 1 }} whileHover={{ scale: 0.98 }}>
-					<Card
-						isPressable
-						onPress={() => {
-							window.open('https://www.roblox.com/games/15127205237/');
-						}}
-						className='max-w-sm md:max-w-xs'
-					>
-						<Image src='/dte-cover.webp' />
-						<CardFooter className='flex flex-col items-start gap-3 p-5'>
-							<div className='flex flex-row justify-between'>
-								<p className='text-xl'>Down to Earth</p>
-							</div>
-							<p
-								className='text-md text-default-500 -mt-2 text-left'
-								color='secondary.main'
-							>
-								A single-player game centered around the theme of green energy.
-							</p>
-							<div className='mt-1 flex flex-wrap gap-2'>
+			<motion.div initial={{ scale: 1 }} whileHover={{ scale: 0.98 }}>
+				<Card
+					isPressable
+					onPress={() => window.open(url)}
+					className='max-w-sm md:max-w-xs w-full'
+				>
+					<Image src={image} height={'180px'} width={'400px'} />
+					<CardFooter className='flex flex-col items-start gap-3 p-5'>
+						<div className='flex flex-row justify-between'>
+							<p className='text-xl'>{gameName}</p>
+						</div>
+						<p
+							className='text-md text-default-500 -mt-2 text-left'
+							color='secondary.main'
+						>
+							{gameDesc}
+						</p>
+						<div className='mt-1 flex flex-wrap gap-2'>
+							{gameTag.map((element) => (
 								<Chip variant='flat' color='primary'>
-									First-Person Shooter
+									{element}
 								</Chip>
-								<Chip variant='flat' color='primary'>
-									Adventure
-								</Chip>
-							</div>
-						</CardFooter>
-					</Card>
-				</motion.div>
-				
-			</div>
+							))}
+						</div>
+					</CardFooter>
+				</Card>
+			</motion.div>
 		</>
 	);
 }
